7c90f134eede8debf98ce47c3e057e6cc55d3e18
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-02-19 09:39:46 +0100

    Exemplo Array Bidimensional

---
+++ ExemploArrayBidimensional/src/exemploarraybidimensional/ExemploArrayBidimensional.java
@@ -0,0 +1,17 @@
+package exemploarraybidimensional;
+
+public class ExemploArrayBidimensional {
+
+    public static void main(String[] args) {
+        Notas notas= new Notas();
+        
+        notas.crearTaboa();
+        notas.amosar();
+        notas.mediaAlumno2();
+        notas.mediaAlumno();
+        //notas.mediaModulo();
+        notas.ordenar();
+        notas.amosar();
+    }
+    
+}

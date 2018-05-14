e87b0571f001fb443192f447197a36ff5d50e0a6
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-05-14 08:37:23 +0200

    Exemplo Paleta

---
+++ ExemploPaleta/src/exemplopaleta/ExemploPaleta.java
@@ -0,0 +1,12 @@
+package exemplopaleta;
+
+public class ExemploPaleta {
+
+    public static void main(String[] args) {
+        Ventana pagina1 = new Ventana();
+        //pagina1.setVisible(true);
+        Saudo pagina2 = new Saudo();
+        pagina2.setVisible(true);
+    }
+    
+}
